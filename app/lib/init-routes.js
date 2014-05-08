40c9b3386f03690fbbd10fd5be5cacbb279c06bd
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-05-08 08:42:05 -0500

    demo day

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -14,8 +14,15 @@ module.exports = function(req, res, next){
 
 function load(app, fn){
   var home = require('../routes/home');
+  var dog = require('../routes/dog');
 
   app.get('/', d, home.index);
+  app.get('/about', home.about);
+  app.get('/faq', home.faq);
+  app.get('/photo', home.photo);
+
+  app.get('/dogs', dog.index);
+
   console.log('Routes Loaded');
   fn();
 }
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,19 @@
+
 'use strict';
 
 exports.index = function(req, res){
-  res.render('home/index', {title: 'Welcome to Node.js'});
+  res.render('home/index', {name:'Bob', age:7, gender:'Maile', title: 'Welcome to Node.js'});
+};
+
+exports.about = function(req, res){
+  res.render('home/about', {title: 'About Node.js'});
+};
+
+exports.faq = function(req, res){
+  res.render('home/faq', {title: 'FAQs Node.js'});
+};
+
+exports.photo = function(req, res){
+  var pics = ['node1.jpg', 'node2.jpg', 'node3.png'];
+  res.render('home/photo', {pics: pics, title: 'Photos Node.js'});
 };
